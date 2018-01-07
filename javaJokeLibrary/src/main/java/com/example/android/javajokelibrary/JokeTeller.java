e17d24d6286b79772392664901c216cab5d936df
Hrishikesh Kadam <hrkadam.92@gmail.com>
2018-01-07 15:58:04 +0530

    Create a java library

---
+++ javaJokeLibrary/src/main/java/com/example/android/javajokelibrary/JokeTeller.java
@@ -0,0 +1,8 @@
+package com.example.android.javajokelibrary;
+
+public class JokeTeller {
+
+    public static String getJoke() {
+        return "My wife and I were happy for 20 years; then we met.";
+    }
+}
